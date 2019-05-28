e6500a6151ee068bfb771383ace7de06347a793b
vlad1211 <51097262+vlad1211@users.noreply.github.com>
2019-05-28 10:18:46 +0300

    Add files via upload

---
+++ Lines2D_test.c
@@ -0,0 +1,23 @@
+/* File  Lines2D_test.c
+Realization of tests for Lines2D structure functions
+Done by Platonov Denis and Oleksenko Vladisval  (group Statistic)
+Email: ... & vlad.oleksenko99@gmail.com
+*/
+#include "Lines2D.c"
+
+int main()
+{
+    struct Point2D Points[4] = {{0,0},{0,2},{2,2},{2,0}};
+    int N = 4;
+    struct Polygone2D pol = {N, Points};
+    double res = polygon_square(pol);
+    int res_2 = check_convex(pol);
+    printf("Polygon square is %f \n", res);
+    if (res_2)
+        printf("The polygon is convex \n");
+
+    else
+        printf("The polygon is not convex \n");
+
+    return 0;
+}
